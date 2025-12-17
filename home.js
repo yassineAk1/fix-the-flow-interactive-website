c8564c7f98a1d89042d333fbf80fb3743b435efe
yassineAk1 <yassine9a6@gmail.com>
2025-12-17 23:26:05 +0100

    #21 homepagina icons gewijzigd

--- home.js
+++ home.js
@@ -2,24 +2,22 @@
 const buttons = document.querySelectorAll(".add-list");
 
 // STAP 2: Loop door alle buttons en voegt event listeners toe
-buttons.forEach(function(button) {
-  
-  button.addEventListener("click", function() {
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
     // Selecteert het icon binnen deze specifieke button
     const iconContent = button.querySelector(".saveButtonHome");
-    
+
     // Toggle de class
     const isSaved = button.classList.toggle("saved");
-    
-// STAP 3: geeft feedback aan de gebruiker 
+
+    // STAP 3: geeft feedback aan de gebruiker
     // Verandert het icon
     if (isSaved) {
-      iconContent.src = "assets/saved-logo.svg";
+      iconContent.src = "assets/check.svg";
     } else {
-      iconContent.src = "assets/newadd.svg";
+      iconContent.src = "assets/addlist.svg";
     }
-    
+
     console.log("button clicked");
   });
-  
 });
